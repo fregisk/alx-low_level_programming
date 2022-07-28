52f191a829b309c47c19d010c91ec291e4fac114
fregisk <fregiskonan34@gmail.com>
2022-07-28 16:42:13 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,24 +1,41 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * _calloc - function that allocates memory for an array, using malloc.
- * @nmemb: number of elements in the array
- * @size: size in bytes of the elements
- *
- * Return: void pointer to allocated memory
+ * string_nconcat - function that concatenates two strings
+ * wich contains s1 followed by the first n bytes of s2
+ * @s1: string number 1
+ * @s2: string number 2
+ * @n: number of first bytes to concatenate
+ * Return: returns the pointer to the allocated memory
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pointer;
-	unsigned int i;
+	unsigned int i, j, s1_length;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	pointer = malloc(nmemb * size);
+	if (s1 == NULL)
+		i = 0;
+	else
+		for (i = 0; s1[i]; i++)
+			;
+	if (s2 == NULL)
+		j = 0;
+	else
+		for (j = 0; s2[j]; j++)
+			;
+	if (n >= j)
+		n = j;
+	pointer = malloc(sizeof(char) * (i + n + 1));
 	if (pointer == NULL)
 		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
-		pointer[i] = 0;
+	s1_length = i;
+	for (i = 0; i < s1_length; i++)
+		pointer[i] = s1[i];
+	for (j = 0; j < n; j++)
+	{
+		pointer[i] = s2[j];
+		i++;
+	}
+	pointer[i] = '\0';
 	return (pointer);
 }
